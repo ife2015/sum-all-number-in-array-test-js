975d6d2ef31f4b224e064d1c68963065ffa65eae
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-19 01:16:12 +0000

    Sum Array Indeex

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0; 
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
